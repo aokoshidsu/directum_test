72cef54411707dc77b6885b2a98642b6ff4ceb5a
aokoshidsu <cairo.ivanova@gmail.com>
2023-05-10 11:53:41 +0400

    files added

---
+++ тестовое задание/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace тестовое_задание
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.minWordCount = new System.Windows.Forms.NumericUpDown();
+            this.maxWordCount = new System.Windows.Forms.NumericUpDown();
+            this.textMin = new System.Windows.Forms.Label();
+            this.textMax = new System.Windows.Forms.Label();
+            this.cntFiles = new System.Windows.Forms.NumericUpDown();
+            this.fileCount = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.generate = new System.Windows.Forms.Button();
+            this.folderShower = new System.Windows.Forms.TextBox();
+            this.folderSearcher = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.minWordCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxWordCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cntFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // minWordCount
+            // 
+            this.minWordCount.Location = new System.Drawing.Point(229, 109);
+            this.minWordCount.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.minWordCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.minWordCount.Name = "minWordCount";
+            this.minWordCount.Size = new System.Drawing.Size(117, 20);
+            this.minWordCount.TabIndex = 0;
+            this.minWordCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // maxWordCount
+            // 
+            this.maxWordCount.Location = new System.Drawing.Point(229, 150);
+            this.maxWordCount.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.maxWordCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.maxWordCount.Name = "maxWordCount";
+            this.maxWordCount.Size = new System.Drawing.Size(117, 20);
+            this.maxWordCount.TabIndex = 1;
+            this.maxWordCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // textMin
+            // 
+            this.textMin.AutoSize = true;
+            this.textMin.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textMin.Location = new System.Drawing.Point(64, 116);
+            this.textMin.Name = "textMin";
+            this.textMin.Size = new System.Drawing.Size(145, 14);
+            this.textMin.TabIndex = 2;
+            this.textMin.Text = "Минимальное кол-во слов";
+            // 
+            // textMax
+            // 
+            this.textMax.AutoSize = true;
+            this.textMax.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textMax.Location = new System.Drawing.Point(64, 157);
+            this.textMax.Name = "textMax";
+            this.textMax.Size = new System.Drawing.Size(150, 14);
+            this.textMax.TabIndex = 3;
+            this.textMax.Text = "Максимальное кол-во слов";
+            // 
+            // cntFiles
+            // 
+            this.cntFiles.Location = new System.Drawing.Point(229, 195);
+            this.cntFiles.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.cntFiles.Name = "cntFiles";
+            this.cntFiles.Size = new System.Drawing.Size(117, 20);
+            this.cntFiles.TabIndex = 4;
+            this.cntFiles.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // fileCount
+            // 
+            this.fileCount.AutoSize = true;
+            this.fileCount.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fileCount.Location = new System.Drawing.Point(64, 197);
+            this.fileCount.Name = "fileCount";
+            this.fileCount.Size = new System.Drawing.Size(160, 14);
+            this.fileCount.TabIndex = 5;
+            this.fileCount.Text = "Кол-во нужных .docx файлов";
+            // 
+            // generate
+            // 
+            this.generate.BackColor = System.Drawing.Color.Black;
+            this.generate.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.generate.FlatAppearance.BorderSize = 0;
+            this.generate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.generate.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generate.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.generate.Location = new System.Drawing.Point(64, 240);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(282, 37);
+            this.generate.TabIndex = 6;
+            this.generate.Text = "Сгенерировать!";
+            this.generate.UseVisualStyleBackColor = false;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // folderShower
+            // 
+            this.folderShower.Enabled = false;
+            this.folderShower.ForeColor = System.Drawing.Color.LightGray;
+            this.folderShower.Location = new System.Drawing.Point(64, 69);
+            this.folderShower.Name = "folderShower";
+            this.folderShower.Size = new System.Drawing.Size(184, 20);
+            this.folderShower.TabIndex = 7;
+            this.folderShower.Text = "Здесь будет путь до вашей папки";
+            // 
+            // folderSearcher
+            // 
+            this.folderSearcher.BackColor = System.Drawing.Color.White;
+            this.folderSearcher.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.folderSearcher.Font = new System.Drawing.Font("Candara", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.folderSearcher.Location = new System.Drawing.Point(254, 68);
+            this.folderSearcher.Name = "folderSearcher";
+            this.folderSearcher.Size = new System.Drawing.Size(92, 23);
+            this.folderSearcher.TabIndex = 8;
+            this.folderSearcher.Text = "Выбор папки...";
+            this.folderSearcher.UseVisualStyleBackColor = false;
+            this.folderSearcher.Click += new System.EventHandler(this.folderSearcher_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Candara", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.title.Location = new System.Drawing.Point(31, 15);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(364, 33);
+            this.title.TabIndex = 9;
+            this.title.Text = "Генератор текстовых файлов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(423, 300);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.folderSearcher);
+            this.Controls.Add(this.folderShower);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.fileCount);
+            this.Controls.Add(this.cntFiles);
+            this.Controls.Add(this.textMax);
+            this.Controls.Add(this.textMin);
+            this.Controls.Add(this.maxWordCount);
+            this.Controls.Add(this.minWordCount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.minWordCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxWordCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cntFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown minWordCount;
+        private System.Windows.Forms.NumericUpDown maxWordCount;
+        private System.Windows.Forms.Label textMin;
+        private System.Windows.Forms.Label textMax;
+        private System.Windows.Forms.NumericUpDown cntFiles;
+        private System.Windows.Forms.Label fileCount;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.TextBox folderShower;
+        private System.Windows.Forms.Button folderSearcher;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ тестовое задание/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xceed.Words.NET;
+using System.IO;
+
+namespace тестовое_задание
+{
+    public partial class Form1 : Form
+    {
+        //Хранит путь до папки, куда нужно генерировать файлы
+        string folderPath = String.Empty;
+        //Путь до базового файла, откуда берутся слова для генерации
+        const string WORDS = @"C:\Users\гser\Desktop\тестовое задание\тестовое задание\RUS.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            minWordCount.TextChanged += minWordCount_TextChanged;
+            maxWordCount.TextChanged += maxWordCount_TextChanged;
+        }
+
+        /// <summary>
+        /// Нажатие на кнопку "Выбрать папку". Сохраняет путь к выбранной папке.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void folderSearcher_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = FBD.SelectedPath;
+                folderShower.Text = FBD.SelectedPath;
+                folderShower.ForeColor = Color.Black;
+            }
+        }
+
+        /// <summary>
+        /// Минимальное значение поля максимум не превышает заданного минимума.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void minWordCount_TextChanged(Object sender, EventArgs e)
+        {
+            maxWordCount.Minimum = minWordCount.Value;
+        }
+
+        /// <summary>
+        /// Максимальное значение поля минимум не превышает заданного максимума.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void maxWordCount_TextChanged(Object sender, EventArgs e)
+        {
+            minWordCount.Maximum = maxWordCount.Value;
+        }
+
+        /// <summary>
+        /// Нажатие на кнопку "Сгенерировать". Генерирует текстовые файлы. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void generate_Click(object sender, EventArgs e)
+        {
+            //Обработки неверно введённых пользователем данных
+            if (String.IsNullOrEmpty(folderPath))
+            {
+                MessageBox.Show("Выберите папку для создания файлов.");
+                return;
+            }
+
+            if ((int)cntFiles.Value == 0)
+            {
+                MessageBox.Show("Кол-во создаваемых файлов не может равняться 0.");
+                return;
+            }
+
+            //Алгоритм генерации текстовых файлов
+            int lineCount = File.ReadLines(WORDS).Count();
+            Random rnd = new Random();
+
+            for (int i = 0; i < (int)cntFiles.Value; i++)
+            {
+                //Создается документ, рандомно определяется кол-во слов в нём
+                int wordCount = rnd.Next((int)minWordCount.Value, (int)maxWordCount.Value);
+                DocX document = DocX.Create(folderPath + $@"\{i+1}.docx");
+                string toInsert = String.Empty;
+
+                //Считываем случайные слова из базового файла, расставляем знаки препинания
+                for (int j = 0; j < wordCount; j++)
+                {
+                    int line = rnd.Next(lineCount);
+                    toInsert += File.ReadLines(WORDS, Encoding.GetEncoding(1251)).Skip(line).First();
+                    toInsert += getSymbol();
+                }
+
+                //Вставляем текст в файл, сохраняем
+                document.InsertParagraph(toInsert);
+                document.Save();
+            }
+
+            MessageBox.Show("Ваши файлы успешно сгенерированы!");
+        }
+
+        /// <summary>
+        /// Функция возвращает рандомный знак препинания для текста.
+        /// </summary>
+        /// <returns></returns>
+        private string getSymbol()
+        {
+            // . , ! ? : - ;
+            Random rand = new Random();
+            int a = rand.Next(50);
+            if (a < 15 || a > 29)
+                return " ";
+            if (a == 29)
+                return "; ";
+            if (a == 28)
+                return " - ";
+            if (a == 27)
+                return ": ";
+            if (a == 26 || a == 25)
+                return "? ";
+            if (a == 24 || a == 23)
+                return "! ";
+            if (a < 23 && a > 19)
+                return ", ";
+            if (a < 20 && a > 14)
+                return ". ";
+            return String.Empty;
+        }
+
+    }
+}
